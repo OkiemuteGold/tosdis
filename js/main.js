b3a94290f0cb903178cfff93361fe6635b8e8d30
Okiemute Gold <goldokiemute1@gmail.com>
2021-09-13 14:10:20 +0100

    cleaned code and file

--- js/main.js
+++
@@ -1,7 +0,0 @@
-let dropDownToggler = document.querySelector(".drop_down .nav_link");
-let subMenu = document.querySelector(".sub_menu");
-
-dropDownToggler.addEventListener("click", () => {
-    subMenu.classList.toggle("display_block");
-    console.log("clicked");
-})
